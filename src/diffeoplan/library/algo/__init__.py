f96b56b7fa5c7759d310984a7d8046b7f80b2b89
adamnilsson <adam.nilss@gmail.com>
2012-08-09 10:08:13 -0700

    renamed random to not confilict with random number gennerator module

--- src/diffeoplan/library/algo/__init__.py
+++ src/diffeoplan/library/algo/__init__.py
@@ -2,5 +2,6 @@ from .. import contract, np, logger
 
 from .planning_result import *
 from .planning_algo import * 
-from .random import *
+from .random_plan import *
 from .graphsearch import *
+from .randomized_expansion import *
